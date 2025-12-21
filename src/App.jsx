ef95cf62862133172821e32de9535914eb7ba5a8
VISHNUVARTHAN U <vvarthan7@gmail.com>
2025-12-21 18:51:22 +0530

    Add Resume window with PDF viewer and download
    Introduced a new Resume window using react-pdf to display and allow downloading of the resume PDF. Updated Navbar to open the Resume window, registered Resume in the windows index, and added react-pdf as a dependency.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Navbar, Welcome, Dock } from "#components";
-import { Safari, Terminal } from "#windows";
+import { Resume, Safari, Terminal } from "#windows";
 import gsap from "gsap";
 
 import { Draggable } from "gsap/Draggable";
@@ -15,6 +15,7 @@ const App = () => {
 
       <Terminal />
       <Safari />
+      <Resume />
     </main>
   );
 };
