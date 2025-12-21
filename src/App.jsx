f16fa54e50bf8b87b2b86ec767c8890efbde6f77
VISHNUVARTHAN U <vvarthan7@gmail.com>
2025-12-21 19:48:07 +0530

    Add Finder window component
    Introduces a new Finder window component to the application, updates the main App to include Finder, and exports it from the windows index. Also adds the 'clsx' dependency for conditional class names.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Navbar, Welcome, Dock } from "#components";
-import { Resume, Safari, Terminal } from "#windows";
+import { Resume, Safari, Terminal, Finder } from "#windows";
 import gsap from "gsap";
 
 import { Draggable } from "gsap/Draggable";
@@ -16,6 +16,7 @@ const App = () => {
       <Terminal />
       <Safari />
       <Resume />
+      <Finder />
     </main>
   );
 };
